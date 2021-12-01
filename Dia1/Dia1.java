9cf1f823de923cfdc4cd3522ea4373361f85e2be
Abbissa <carlospozoserrano13@gmail.com>
2021-12-01 17:16:24 +0100

    Update Dia1.java

--- Dia1/Dia1.java
+++ Dia1/Dia1.java
@@ -1,26 +1,24 @@
 import java.io.BufferedInputStream;
-
 import java.io.File;
 import java.util.Scanner;
 
 public class Dia1 {
 
 	public static void main(String[] args) {
-		
+
 		Part1();
+
 		//Part2();
 	}
 	private static void Part1() {
 		int prev=-1;
 		int act=1;
 		int cont=0;
-        
-		try{
 
-         Scanner sc = new Scanner(new File("./Input.txt")); 
+		try (Scanner sc = new Scanner(new File(".\\Input.txt"))) {
 			while(sc.hasNextInt()) {
 				act=sc.nextInt();
-                System.out.println(act);
+				System.out.println(act);
 				if(prev!=-1&&prev<act) {
 					cont++;
 				}
